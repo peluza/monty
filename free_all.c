9f599bb82d41941e14b05e5746a6b025a551b864
peluza <edisonisaza@gmail.com>
2020-05-15 00:15:32 -0500

    0-push-pall

--- free_all.c
+++ free_all.c
@@ -1,7 +1,12 @@
 #include "monty.h"
-
+/**
+ *free_all - free all
+ *@entrada: characters is punters at punters
+ *@stack: characters is the struct
+ * Return: Always 0 (Success)
+ */
 void free_all(char **entrada, stack_t *stack)
 {
-    free_line(entrada);
+	free_line(entrada);
 	free_stack_t(stack);
 }
