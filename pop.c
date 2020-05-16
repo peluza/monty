d4ac9a8ff1c56976f03d9a51239ba5ed206792ec
peluza <edisonisaza@gmail.com>
2020-05-16 15:26:36 -0500

    1-pop

--- pop.c
+++ pop.c
@@ -1,5 +1,10 @@
 #include "monty.h"
-
+/**
+ *pop - delete in the elemnt 0 on the lists
+ *@stack: this is variable of the struct
+ *@line_number: number of the line
+ * Return: Always EXIT_SUCCES
+ */
 void pop(stack_t **stack, unsigned int line_number)
 {
     delete_dnodeint_at_index(stack, line_number);
